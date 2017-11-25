5789a28814fd19cd8b236dd3699dcea2d9d00f20
bae jiun, maybe <alice.maydev@gmail.com>
2017-11-26 06:02:15 +0900

    Update Tests size
    Down LinkedList test size

--- ParBST/test/collections/LinkedListTest.java
+++ ParBST/test/collections/LinkedListTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.*;
 public class LinkedListTest {
     private static collections.interfaces.List<Integer> list;
 
-    private static final int testSize = 1000000;
+    private static final int testSize = 10000;
     private static List<Integer> numbers;
 
     @BeforeClass
@@ -27,19 +27,17 @@ public class LinkedListTest {
     @Before
     public void makeInstance() throws Exception {
         list = new collections.LinkedList<>();
-
-        for (int i : numbers) {
-            list.add(i);
-        }
     }
 
     @Test
     public void add() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         assertEquals(numbers.size(), list.size());
     }
 
     @Test
     public void add1() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         int index = 3;
         list.add(index, numbers.get(index));
 
@@ -48,28 +46,33 @@ public class LinkedListTest {
 
     @Test
     public void addFirst() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         list.addFirst(numbers.get(0));
         assertEquals(numbers.get(0), list.getFirst());
     }
 
     @Test
     public void get() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         for (int i = 0; i < numbers.size() / 1000; ++i)
             assertEquals(numbers.get(i), list.get(i));
     }
 
     @Test
     public void getFirst() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         assertEquals(numbers.get(0), list.getFirst());
     }
 
     @Test
     public void getLast() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         assertEquals(numbers.get(numbers.size() - 1), list.getLast());
     }
 
     @Test
     public void indexOf() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         for (int i = 0; i < numbers.size() / 1000; ++i) {
             assertEquals(i, list.indexOf(numbers.get(i)));
         }
@@ -77,24 +80,27 @@ public class LinkedListTest {
 
     @Test
     public void remove() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         list.remove(0);
         assertEquals(numbers.size() - 1, list.size());
     }
 
     @Test
     public void remove1() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         numbers.forEach((e) -> list.remove(e));
-
         assertEquals(0, list.size());
     }
 
     @Test
     public void size() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         assertEquals(numbers.size(), list.size());
     }
 
     @Test
     public void toArray() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         Object[] array = list.toArray();
         for (int i = 0; i < numbers.size(); ++i) {
             assertEquals(array[i], numbers.get(i));
@@ -103,6 +109,7 @@ public class LinkedListTest {
 
     @Test
     public void iterate() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         int counter = 0;
         for (Iterator it = list.iterator(); it.hasNext();) {
             Integer item = (Integer) it.next();
@@ -112,6 +119,7 @@ public class LinkedListTest {
 
     @Test
     public void forEach() throws Exception {
+        numbers.forEach((e) -> list.add(e));
         int counter = 0;
         for (Integer i : list) {
             assertEquals(numbers.get(counter++), i);
