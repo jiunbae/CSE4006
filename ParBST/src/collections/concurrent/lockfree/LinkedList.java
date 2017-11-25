9265c25bb7d99989b5f89ed5436aa9d23a996272
bae jiun, maybe <alice.maydev@gmail.com>
2017-11-26 05:25:09 +0900

    Update concurrent.lockfree.LinkedList

--- ParBST/src/collections/concurrent/lockfree/LinkedList.java
+++ ParBST/src/collections/concurrent/lockfree/LinkedList.java
@@ -6,18 +6,18 @@ import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class LinkedList<T extends Comparable<? super T>> implements List<T> {
     private Node<T> head;
-    private Node<T> tail;
+    private Node<T> last;
     private int counter;
 
     class Node<F extends Comparable<? super F>> extends List.Node<F> {
+        int key;
         F item;
-        AtomicMarkableReference<Node<F>> front;
         AtomicMarkableReference<Node<F>> next;
 
         public Node(F item) {
             super(item);
             this.item = item;
-            front = new AtomicMarkableReference<>(null, false);
+            if (item != null) this.key = item.hashCode();
             next = new AtomicMarkableReference<>(null, false);
         }
     }
@@ -30,7 +30,7 @@ public class LinkedList<T extends Comparable<? super T>> implements List<T> {
         }
     }
 
-    public Window<T> find(Node<T> head, T item) {
+    public Window<T> find(int key) {
         Node<T> pre;
         Node<T> cur;
         Node<T> suc;
@@ -48,7 +48,7 @@ public class LinkedList<T extends Comparable<? super T>> implements List<T> {
                     suc = cur.next.get(marked);
                 }
 
-                if (cur.item.compareTo(item) < 0)
+                if (cur.key >= key)
                     return new Window<>(pre, cur);
                 pre = cur;
                 cur = suc;
@@ -56,73 +56,121 @@ public class LinkedList<T extends Comparable<? super T>> implements List<T> {
         }
     }
 
+    public LinkedList() {
+        head = new Node<>(null);
+        head.key = Integer.MIN_VALUE;
+        last = new Node<>(null);
+        last.key = Integer.MAX_VALUE;
+        head.next.set(last, false);
+        last.next.set(null, false);
+        counter = 0;
+    }
     @Override
     public boolean add(int index, T item) {
-        return false;
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
     }
 
     @Override
     public boolean add(T item) {
-        return false;
+        int key = item.hashCode();
+        while (true) {
+            Window<T> window = find(key);
+            Node<T> pre = window.pre;
+            Node<T> cur = window.cur;
+
+            if (cur.key == key) return false;
+            else {
+                Node<T> node = new Node<>(item);
+                node.next = new AtomicMarkableReference<>(cur, false);
+                if (pre.next.compareAndSet(cur, node, false, false))
+                    return true;
+            }
+        }
     }
 
     @Override
     public boolean addFirst(T item) {
-        return false;
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
     }
 
     @Override
     public Node<T> addNode(T item) {
-        return null;
-    }
-
-    @Override
-    public T get(int index) {
-        return null;
-    }
-
-    @Override
-    public T getFirst() {
-        return null;
-    }
-
-    @Override
-    public T getLast() {
-        return null;
-    }
-
-    @Override
-    public int indexOf(T item) {
-        return 0;
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
     }
 
     @Override
     public boolean remove(T item) {
-        return false;
+        int key = item.hashCode();
+        boolean snip;
+        while (true) {
+            Window<T> window = find(key);
+            Node<T> pre = window.pre;
+            Node<T> cur = window.cur;
+            if (cur.key != key) return false;
+            else {
+                Node<T> suc = cur.next.getReference();
+                snip = cur.next.attemptMark(suc, true);
+                if (!snip) continue;
+                pre.next.compareAndSet(cur, suc, false, false);
+                return true;
+            }
+        }
     }
 
     @Override
     public <N extends List.Node<T>> T removeNode(N node) {
-        return null;
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
+    }
+
+    @Override
+    public T get(int index) {
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
+    }
+
+    @Override
+    public T getFirst() {
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
+    }
+
+    @Override
+    public T getLast() {
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
+    }
+
+    @Override
+    public boolean contains(T item) {
+        boolean[] marked = {false};
+        int key = item.hashCode();
+        Node<T> cur = head;
+        while (cur.key < key) {
+            cur = cur.next.getReference();
+            Node suc = cur.next.get(marked);
+        }
+        return (cur.key == key && !marked[0]);
+    }
+
+    @Override
+    public int indexOf(T item) {
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
     }
 
     @Override
     public int size() {
-        return 0;
+        return counter;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
     }
 
     @Override
     public Iterator iterator() {
-        return null;
+        throw new UnsupportedOperationException("Not support in lockfree.LinkedList");
     }
 }
