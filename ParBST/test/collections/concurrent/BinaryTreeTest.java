82034ca523d0e99a6520fe4cb0da222b41338034
bae jiun, maybe <alice.maydev@gmail.com>
2017-11-28 01:22:27 +0900

    Add test, BinaryTreeTest#complex

--- ParBST/test/collections/concurrent/BinaryTreeTest.java
+++ ParBST/test/collections/concurrent/BinaryTreeTest.java
@@ -75,4 +75,33 @@ public class BinaryTreeTest {
         numbers.forEach((e) -> pool.push(() -> assertTrue(tree.search(e))));
         pool.join();
     }
+
+    @Test
+    public void complex() throws Exception {
+        Set<Integer> verifier = new HashSet<>();
+        Random random = new Random(0);
+        numbers.forEach((e) -> {
+            tree.insert(e);
+            verifier.add(e);
+        });
+        Collections.shuffle(numbers);
+        for (int i = 0; i < testSize; ++i) {
+            int finalI = i;
+            switch (random.nextInt(3)) {
+                case 0:
+                    pool.push(() -> tree.insert(numbers.get(finalI)));
+                    verifier.add(numbers.get(finalI));
+                    break;
+                case 1:
+                    pool.push(() -> tree.delete(numbers.get(finalI)));
+                    verifier.remove(numbers.get(finalI));
+                    break;
+                case 2:
+                    pool.push(() -> tree.search(numbers.get(finalI)));
+                    break;
+            }
+        }
+        pool.join();
+        numbers.forEach((e) -> assertEquals(verifier.contains(e), tree.search(e)));
+    }
 }
