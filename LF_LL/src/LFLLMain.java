522f23b23c091be58b164d0b2b60e1194fdfb5df
bae jiun, maybe <alice.maydev@gmail.com>
2017-11-28 22:49:30 +0900

    Update build Main

--- LF_LL/src/LFLLMain.java
+++ LF_LL/src/LFLLMain.java
@@ -1,30 +1,72 @@
 import collections.concurrent.lockfree.LinkedList;
-import collections.interfaces.List;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Main {
-
-
-    private static List<Integer> list;
+public class LFLLMain {
+    private static collections.interfaces.List<Integer> list;
     private static concurrent.Pool pool;
 
+    private static final int[] threadSizes = new int[]{ 1, 2, 4, 8 };
+    private static final int[] searchRatio = new int[]{ 1, 4, 9 };
     private static final int testSize = 10000;
-    private static java.util.List<Integer> numbers;
+    private static List<Integer> numbers;
 
     public static void main(String[] args) {
-        list = new LinkedList<>();
-
         numbers = IntStream.range(0, testSize).boxed().collect(Collectors.toList());
         Collections.shuffle(numbers);
 
-        pool = new concurrent.Pool(4);
+        for (int threadSize : threadSizes) {
+            list = new LinkedList<>();
+            pool = new concurrent.Pool(threadSize);
 
-        numbers.forEach((e) -> pool.push(() -> list.add(e)));
-        pool.join();
-        System.out.println("Done");
+            System.out.println(String.format("Test Start with Thread %d", threadSize));
+            try {
+                testA();
+                testB();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
+    private static long executeWithTime(Runnable task) {
+        long start = System.currentTimeMillis();
+        task.run();
+        return System.currentTimeMillis() - start;
+    }
+
+    private static boolean assertRatio(int t, int f, int v) {
+        return (v % (t + f)) < t;
+    }
+
+    static void testA() throws Exception {
+        System.out.println(String.format("TestA: Insert %d numbers", numbers.size()));
+        long time = executeWithTime(() -> {
+            numbers.forEach((e) -> pool.push(() -> list.add(e)));
+            pool.join();
+        });
+        System.out.println(String.format("\tInserting %d numbers takes %dms", numbers.size(), time));
+    }
+
+
+    static void testB() throws Exception {
+        long insertTime = executeWithTime(() -> {
+            numbers.forEach((e) -> pool.push(() -> list.add(e)));
+            pool.join();
+        });
+        System.out.println(String.format("TestB: Insert and Search %d with ratio", numbers.size()));
+        for (int ratio : searchRatio) {
+            long time = executeWithTime(() -> {
+                numbers.forEach((e) -> {
+                    if (assertRatio(1, ratio, e)) pool.push(() -> list.add(e));
+                    else pool.push(() -> list.contains(e));
+                });
+                pool.join();
+            });
+            System.out.println(String.format("\tInsert and Search ratio 1:%d, %d numbers takes %dms", ratio, numbers.size(), time));
+        }
+    }
 }
